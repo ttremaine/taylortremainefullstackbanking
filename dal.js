26d7405231719483027efc44c22747f61dd46c15
Taylor Tremaine <76922507+ttremaine@users.noreply.github.com>
2021-10-25 23:37:36 -0400

    Add files via upload

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = 'mongodb+srv://doadmin:3bB2N9u40w17S8Cg@db-mongodb-nyc1-43943-38d4ad2f.mongo.ondigitalocean.com:27017';
 let db            = null;
  
 // connect to mongo
