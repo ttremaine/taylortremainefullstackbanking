23a219f9811aa5976f70ecfdcfae3787908123d4
Taylor Tremaine <76922507+ttremaine@users.noreply.github.com>
2021-11-08 05:28:01 -0500

    Add files via upload

--- dal.js
+++ dal.js
@@ -1,14 +1,42 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb+srv://admin:<kYGq6vU8IPCrQTui>@cluster0.qfhsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const url         = 'mongodb+srv://admin:<kYGq6vU8IPCrQTui>@cluster0.qfhsi.mongodb.net/myproject?retryWrites=true&w=majority';
 let db            = null;
  
 // connect to mongo
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
     console.log("Connected successfully to db server");
 
     // connect to myproject database
-    db = client.db('Cluster0');
+    db = client.db('myproject');
 });
+
+/*const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<kYGq6vU8IPCrQTui>@cluster0.qfhsi.mongodb.net/myproject?retryWrites=true&w=majority"; 
+const client = new MongoClient(uri, { useNewUrlParser: true }); 
+client.connect(err => { const db = client.db("myproject") }); 
+console.log("Database Connected");*/
+
+/*const MongoClient = require('mongodb').MongoClient;
+const test = require('assert');
+// Connection url
+const url = 'mongodb+srv://admin:<kYGq6vU8IPCrQTui>@cluster0.qfhsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// Database Name
+const dbName = 'test';
+// Connect using MongoClient
+const mongoClient = new MongoClient(url);
+mongoClient.connect(function(err, client) {
+  const db = client.db(dbName);
+  client.close();
+});*/
+
+/*const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:<kYGq6vU8IPCrQTui>@cluster0.qfhsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
 
 // create user account
 function create(name, email, password){
